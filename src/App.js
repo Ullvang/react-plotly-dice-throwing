804eaeac6fbe0cf9e1223d922d01faacf6be9ec8
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-12-19 22:38:48 +0100

    made plot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PlotComponent from "./components/PlotComponent";
+import "./App.css";
 
 function App() {
+  const [dice, setDice] = useState([]);
+  const [throws, setThrows] = useState(5000);
+  const [numberOfDice, setNumberOfDice] = useState(5);
+  const [numberOfDiceAsProp, setNumberOfDiceAsProp] = useState(0);
+
+  const rollDice = () => {
+    let result = [];
+    for (let i = 0; i < throws; i++) {
+      let throwResult = [];
+      for (let j = 0; j < numberOfDice; j++) {
+        throwResult.push(Math.floor(Math.random() * 6) + 1);
+      }
+      result.push(throwResult);
+    }
+    setDice(result);
+    setNumberOfDiceAsProp(numberOfDice);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={rollDice}>Roll dice</button>
+      <br />
+      <h3>How many throws?</h3>
+      <p>{throws}</p>
+      <input
+        style={{ marginTop: "20px" }}
+        type="range"
+        name="numberOfDices"
+        min="1"
+        max="10000"
+        value={throws}
+        onChange={(e) => setThrows(e.target.value)}
+      />
+      <h3>How many dice in each throw?</h3>
+      <p>{numberOfDice}</p>
+      <input
+        style={{ marginTop: "20px" }}
+        type="range"
+        name="numberOfDices"
+        min="1"
+        max="10"
+        value={numberOfDice}
+        onChange={(e) => setNumberOfDice(e.target.value)}
+      />
+      {dice.length > 0 ? (
+        <PlotComponent dice={dice} numberOfDice={numberOfDiceAsProp} />
+      ) : null}
     </div>
   );
 }
---
+++ src/components/PlotComponent.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Plot from "react-plotly.js";
+
+export default function PlotComponent(props) {
+  const { dice, numberOfDice } = props;
+  const combinedValueOfThrow = [];
+  let combinedValueOfThrowCount = {};
+
+  for (var i = numberOfDice; i <= numberOfDice * 6; i++) {
+    combinedValueOfThrow.push(i);
+    combinedValueOfThrowCount[i] = 0;
+  }
+  for (let die of dice) {
+    let sum = die.reduce((a, b) => a + b);
+    combinedValueOfThrowCount[sum]++;
+  }
+  const count = Object.values(combinedValueOfThrowCount);
+
+  const innerWidth = window.innerWidth;
+
+  var result = {
+    type: "bar",
+    hoverinfo: "none",
+    text: count.map(String),
+    textposition: "outside",
+  };
+
+  if (innerWidth < 500) {
+    result.orientation = "h";
+    result.x = count;
+    result.y = combinedValueOfThrow;
+  } else {
+    result.x = combinedValueOfThrow;
+    result.y = count;
+  }
+
+  const layout = {
+    title: {
+      text: "Combined value of each throw and count",
+    },
+    barmode: "stack",
+    xaxis: {
+      title: "Combined value of each throw",
+    },
+    yaxis: {
+      title: "Count of each sum occurring",
+    },
+  };
+
+  if (innerWidth < 500) {
+    layout.yaxis.tickmode = "linear";
+    layout.yaxis.title = "Combined value of each throw";
+    layout.xaxis.title = "Count of each sum occurring";
+  } else {
+    layout.xaxis.tickmode = "linear";
+  }
+
+  return (
+    <Plot
+      data={[result]}
+      layout={layout}
+      style={{
+        width: "100%",
+        height: 800,
+      }}
+      config={{ displayModeBar: false }}
+    />
+  );
+}
